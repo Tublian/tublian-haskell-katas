66dd58c3779c32d5161335d39957602d02db1884
nraychaudhuri <nraychaudhuri@gmail.com>
2023-02-01 17:33:40 -0500

    Added shell script to run the tests

---
+++ runTests.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+stack test 2>&1 | tee ./.tublian/output
+
+result=$(cat ./.tublian/output | grep '[0-9]* examples, [0-9]* failures')
+error=$(cat ./.tublian/output | grep 'error')
+username=`git config --get user.name`
+
+curl -s -d "result=${result}&username=${username}&error=${error}" -X POST "https://httpbin.org/post" > ./.tublian/http
